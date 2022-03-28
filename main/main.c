6f2c1d7ce20395619d550792818cbd30b951d0e3
kmatsu <kmatsu.pro@gmail.com>
2022-03-28 17:35:18 +0900

    configure ili9341

--- main/main.c
+++ main/main.c
@@ -29,19 +29,23 @@
 
 #include "lvgl_helpers.h"
 
-#ifndef CONFIG_LV_TFT_DISPLAY_MONOCHROME
-    #if defined CONFIG_LV_USE_DEMO_WIDGETS
-        #include "lv_examples/src/lv_demo_widgets/lv_demo_widgets.h"
-    #elif defined CONFIG_LV_USE_DEMO_KEYPAD_AND_ENCODER
-        #include "lv_examples/src/lv_demo_keypad_encoder/lv_demo_keypad_encoder.h"
-    #elif defined CONFIG_LV_USE_DEMO_BENCHMARK
-        #include "lv_examples/src/lv_demo_benchmark/lv_demo_benchmark.h"
-    #elif defined CONFIG_LV_USE_DEMO_STRESS
-        #include "lv_examples/src/lv_demo_stress/lv_demo_stress.h"
-    #else
-        #error "No demo application selected."
-    #endif
-#endif
+// #ifndef CONFIG_LV_TFT_DISPLAY_MONOCHROME
+//     #if defined CONFIG_LV_USE_DEMO_WIDGETS
+//         #include "lv_examples/src/lv_demo_widgets/lv_demo_widgets.h"
+//     #elif defined CONFIG_LV_USE_DEMO_KEYPAD_AND_ENCODER
+//         #include "lv_examples/src/lv_demo_keypad_encoder/lv_demo_keypad_encoder.h"
+//     #elif defined CONFIG_LV_USE_DEMO_BENCHMARK
+//         #include "lv_examples/src/lv_demo_benchmark/lv_demo_benchmark.h"
+//     #elif defined CONFIG_LV_USE_DEMO_STRESS
+//         #include "lv_examples/src/lv_demo_stress/lv_demo_stress.h"
+//     #else
+//         #error "No demo application selected."
+//     #endif
+// #endif
+
+#include "lv_examples\src\lv_ex_get_started\lv_ex_get_started.h"
+
+
 
 /*********************
  *      DEFINES
@@ -189,17 +193,18 @@ static void create_demo_application(void)
 #else
     /* Otherwise we show the selected demo */
 
-    #if defined CONFIG_LV_USE_DEMO_WIDGETS
-        lv_demo_widgets();
-    #elif defined CONFIG_LV_USE_DEMO_KEYPAD_AND_ENCODER
-        lv_demo_keypad_encoder();
-    #elif defined CONFIG_LV_USE_DEMO_BENCHMARK
-        lv_demo_benchmark();
-    #elif defined CONFIG_LV_USE_DEMO_STRESS
-        lv_demo_stress();
-    #else
-        #error "No demo application selected."
-    #endif
+//     #if defined CONFIG_LV_USE_DEMO_WIDGETS
+//         lv_demo_widgets();
+//     #elif defined CONFIG_LV_USE_DEMO_KEYPAD_AND_ENCODER
+//         lv_demo_keypad_encoder();
+//     #elif defined CONFIG_LV_USE_DEMO_BENCHMARK
+//         lv_demo_benchmark();
+//     #elif defined CONFIG_LV_USE_DEMO_STRESS
+//         lv_demo_stress();
+//     #else
+//         #error "No demo application selected."
+//     #endif
+lv_ex_get_started_1();
 #endif
 }
 
